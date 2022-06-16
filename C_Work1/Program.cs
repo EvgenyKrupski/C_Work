4beec18867c9514a79402af466f65cea618b75ae
EvgenyKrupski <Krupev87@yandex.ru>
2022-06-16 14:33:52 +0300

    Решение задачи

--- C_Work1/Program.cs
+++ C_Work1/Program.cs
@@ -8,3 +8,28 @@
 
 string[] array1 = new string[] { "111", "hello", "alo", "sister", "ok", "25" };
 string[] array2 = new string[array1.Length];
+
+void SecondArrayItog(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+        
+    }
+
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayItog(array1, array2);
+PrintArray(array2);
